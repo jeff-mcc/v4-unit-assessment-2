4b9fa8877d9c1123f230fb57d127c26065890fce
jeff-mcc <reffery@gmail.com>
2021-04-15 17:29:04 -0600

    completed javascript-1 passing all Jasmine tests

--- javascript-1/javascript-1.js
+++ javascript-1/javascript-1.js
@@ -8,7 +8,7 @@
     Call the array 'myArr'.
 */
 
-//CODE HERE
+const myArr = [4,'abc',['cat','dog','bird'],77];
 
 
 ////////////////////PROBLEM 2////////////////////
@@ -20,7 +20,7 @@
 
 const nestedLetters = ['m', 'g', 'e', 'q', 'h', ['n', 'b', ['v', 'z', 'y', 'r']], 'a']
 
-//CODE HERE
+const foundZ = nestedLetters[5][2][1];
 
 
 ////////////////////PROBLEM 3////////////////////
@@ -36,7 +36,7 @@ const savannah = ['lion', 'zebra', 'lion', 'giraffe']
 const desert = ['rattlesnake', 'coyote']
 //DO NOT EDIT CODE ABOVE
 
-//CODE HERE
+const animals = [...forest,...ocean,...savannah,...desert];
 
 
 /*
@@ -44,7 +44,7 @@ const desert = ['rattlesnake', 'coyote']
     Call the new array 'animalsCopy' and add 'elephant' onto the end of the array.
 */
 
-//CODE HERE
+const animalsCopy = [...animals,'elephant'];
 
 
 ////////////////////PROBLEM 4////////////////////
@@ -55,7 +55,7 @@ const desert = ['rattlesnake', 'coyote']
     If the numbers are the same, just return the number.
 */
 
-//CODE HERE
+let compareNums = (num1,num2) => num1 > num2 ? num1 : num2;
 
   
 ////////////////////PROBLEM 5////////////////////
@@ -67,7 +67,7 @@ const desert = ['rattlesnake', 'coyote']
     we would expect the function to return 'Sharknado is the best movie ever!'
 */
 
-//CODE HERE
+let bestMovie=(movie)=> `${movie} is the best movie ever!`;
   
   
 ////////////////////PROBLEM 6////////////////////
@@ -75,7 +75,7 @@ const desert = ['rattlesnake', 'coyote']
     Write an arrow function called 'jsNinja' that returns the string: 'I am a JavaScript ninja!'
 */
 
-//CODE HERE
+let jsNinja = () => 'I am a JavaScript ninja!'
   
 
 ////////////////////PROBLEM 7////////////////////
@@ -96,7 +96,7 @@ const desert = ['rattlesnake', 'coyote']
     delete the rating property off of the gameInfo object.
 */
 
-//CODE HERE
+delete gameInfo.rating
   
 
 ////////////////////PROBLEM 8////////////////////
@@ -118,7 +118,9 @@ const desert = ['rattlesnake', 'coyote']
     looping over the shapes object and deleting any property whose value is odd number.
 */
 
-//CODE HERE
+for (let key in shapes){
+    shapes[key] % 2 === 0 ? '' : delete shapes[key];
+}
   
   
 ////////////////////PROBLEM 9////////////////////
@@ -160,7 +162,11 @@ const classes = [
     all online classes with no homework.
 */
 
-//CODE HERE
+for (let i = 0; i<classes.length; i++){
+    for (let key in classes[i]){
+        classes[i][key] === true ? classes[i][key] = false : '';
+    }
+}
 
   
 ////////////////////PROBLEM 10////////////////////
@@ -176,9 +182,13 @@ const lettersToPair = ['e', 'k', 's', 'a', 'e', 's', 'a', 'n', 'k', 'n']
 let pairsArray = []
 //DO NOT EDIT CODE ABOVE
 
-//CODE HERE
-
-    
+for (let i = 0; i<lettersToPair.length; i++){
+    for (let j = 0; j<lettersToPair.length; j++){
+        if (lettersToPair[i] === lettersToPair[j] && i<j){
+            pairsArray.push([i,j]);
+        }
+    }
+}   
 
 //////////////////////////////////PROBLEMS 11-14//////////////////////////////////
 /*
@@ -193,7 +203,12 @@ let pairsArray = []
     Those values should come from the functions parameters: name, age, breed, tricks (in order).
 */
 
-//CODE HERE
+function Dog(name, age, breed, tricks){
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+    this.tricks = tricks;
+}
 
 
 /*
@@ -202,7 +217,7 @@ let pairsArray = []
     Store the result in a variable called 'fido'.
 */
 
-//CODE HERE
+const fido = new Dog('Fido',3,'Jack Russell',['sit','shake'])
   
 
 ////////////////////PROBLEM 12////////////////////
@@ -212,7 +227,9 @@ let pairsArray = []
     NAME will come from that context, so you should reference 'this.name' to get the correct name.
 */
 
-//CODE HERE
+function bark(){
+    return `${this.name} says bark!`
+}
 
 
 /*
@@ -220,7 +237,7 @@ let pairsArray = []
     and saving the result to a variable called fidoSpeak.
 */
 
-//CODE HERE
+const fidoSpeak = bark.call(fido)
   
   
 ////////////////////PROBLEM 13////////////////////
@@ -231,7 +248,10 @@ let pairsArray = []
     Tricks will come from that context, so you should reference 'this.tricks' to access the correct array.
 */
 
-//CODE HERE
+function teachTrick(trick){
+    this.tricks.push(trick)
+    return this.tricks
+}
 
 
 /*
@@ -239,7 +259,7 @@ let pairsArray = []
     Save the result to a variable called 'teachStay'.
 */
 
-//CODE HERE
+const teachStay = teachTrick.bind(fido,'stay')
   
   
 ////////////////////PROBLEM 14////////////////////
@@ -250,7 +270,9 @@ let pairsArray = []
     Remember to use the 'this' keyword to access values from the context that you will apply.
 */
 
-//CODE HERE
+function dogIntro(treat,toy){
+    return `${this.name} is a ${this.breed} that loves ${treat} and their ${toy}!`
+}
 
 
 /*
@@ -259,7 +281,7 @@ let pairsArray = []
     and save the result to a variable called fidoIntro.
 */
 
-//CODE HERE
+const fidoIntro = dogIntro.apply(fido,['chicken','tennis ball'])
   
 
 ////////////////////PROBLEM 15////////////////////
@@ -269,7 +291,13 @@ let pairsArray = []
     Those values should come from the function's parameters: brand, model, storage, color, sold (in order).
 */
 
-//CODE HERE
+function Phone(brand,model,storage,color,sold){
+    this.brand = brand;
+    this.model = model;
+    this.storage = storage;
+    this.color = color;
+    this.sold = sold;
+}
 
   
 /*
@@ -284,11 +312,11 @@ let pairsArray = []
 */
 
 //CODE HERE
-  // let phone1 = 
+  let phone1 = new Phone('pear','P1',64,'green',false);
   
-  // let phone2 = 
+  let phone2 = new Phone('mango','M1',128,'orange',false);
   
-  // let phone3 = 
+  let phone3 = new Phone('grapefruit','G1',32,'yellow',false);
   
 /*
     Last, add a prototype method to Phone.
@@ -298,6 +326,9 @@ let pairsArray = []
     Don't forget about the context of BRAND and MODEL.
 */
 
-//CODE HERE
+Phone.prototype.sell = function(){
+    this.sold = true;
+    return `${this.brand} ${this.model} has been sold.`
+}
 
   
